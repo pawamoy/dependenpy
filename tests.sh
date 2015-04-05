2312fc4255a28dd3edf052112448a8d5a645615a
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-04-05 14:37:16 +0200

    Ignoring module import not at top of file pep8

--- tests.sh
+++ tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pep8 --ignore=E226 dependenpy/utils.py &&
+pep8 --ignore=E226,E402 dependenpy/utils.py &&
 pyflakes dependenpy/utils.py || exit 1
 pylint -f colorized -d C0103 dependenpy/utils.py || true
