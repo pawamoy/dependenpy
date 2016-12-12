683fc60c77cebd871b7378598a93fb4d8727d155
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-12-12 16:05:49 +0100

    Clean up

--- src/dependenpy/__init__.py
+++ src/dependenpy/__init__.py
@@ -11,7 +11,7 @@ Dependenpy package.
 
 With dependenpy you will be able to analyze the internal dependencies in
 your Python code, i.e. which module needs which other module. You will then
-ben able to build a dependency matrix and use it for other purposes.
+be able to build a dependency matrix and use it for other purposes.
 """
 
 from __future__ import unicode_literals
