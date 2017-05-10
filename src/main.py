9e7a8d88f3fafeb5f6300145a56bfa679c55a026
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-05-10 21:51:29 +0200

    Clean up

--- src/main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from dependenpy.utils import DSM
-
-dsm = DSM(['django'])
-dsm.build_dependencies()
--- tests/internal/test.py
+++
@@ -1,9 +0,0 @@
-import builtin
-import internal.submodule1
-from internal import submodule2
-from internal.submodule1 import submoduleA
-from internal.submodule1 import submoduleA, test
-from internal.submodule1.submoduleA import test
-from . import test
-from internal.submodule2 import doesnotexists
-from external.exists import something
