5dd9380d51810a89ae9980350fb75a7d0b5c544a
Timothée Mazzucotelli <pawamoy@pm.me>
2022-06-13 19:29:32 +0200

    chore: Template upgrade

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 [![ci](https://github.com/pawamoy/dependenpy/workflows/ci/badge.svg)](https://github.com/pawamoy/dependenpy/actions?query=workflow%3Aci)
 [![documentation](https://img.shields.io/badge/docs-mkdocs%20material-blue.svg?style=flat)](https://pawamoy.github.io/dependenpy/)
 [![pypi version](https://img.shields.io/pypi/v/dependenpy.svg)](https://pypi.org/project/dependenpy/)
+[![gitpod](https://img.shields.io/badge/gitpod-workspace-blue.svg?style=flat)](https://gitpod.io/#https://github.com/pawamoy/dependenpy)
 [![gitter](https://badges.gitter.im/join%20chat.svg)](https://gitter.im/dependenpy/community)
 
 Show the inter-dependencies between modules of Python packages.
@@ -12,42 +13,17 @@ To do this, it reads and searches the source code for import statements.
 
 ![demo](demo.svg)
 
-## Requirements
-
-Dependenpy requires Python 3.6 or above.
-
-<details>
-<summary>To install Python 3.6, I recommend using <a href="https://github.com/pyenv/pyenv"><code>pyenv</code></a>.</summary>
-
-```bash
-# install pyenv
-git clone https://github.com/pyenv/pyenv ~/.pyenv
-
-# setup pyenv (you should also put these three lines in .bashrc or similar)
-export PATH="${HOME}/.pyenv/bin:${PATH}"
-export PYENV_ROOT="${HOME}/.pyenv"
-eval "$(pyenv init -)"
-
-# install Python 3.6
-pyenv install 3.6.12
-
-# make it available globally
-pyenv global system 3.6.12
-```
-</details>
-
 ## Installation
 
 With `pip`:
 ```bash
-python3.6 -m pip install dependenpy
+pip install dependenpy
 ```
 
 With [`pipx`](https://github.com/pipxproject/pipx):
 ```bash
-python3.6 -m pip install --user pipx
-
-pipx install --python python3.6 dependenpy
+python3.7 -m pip install --user pipx
+pipx install dependenpy
 ```
 
 ## Usage (as a library)
--- duties.py
+++ duties.py
@@ -1,17 +1,19 @@
 """Development tasks."""
 
+import importlib
 import os
 import re
 import sys
-from functools import wraps
+import tempfile
+from contextlib import suppress
+from io import StringIO
 from pathlib import Path
-from shutil import which
 from typing import List, Optional, Pattern
 from urllib.request import urlopen
 
 from duty import duty
 
-PY_SRC_PATHS = (Path(_) for _ in ("src", "tests", "duties.py", "docs/*.py"))
+PY_SRC_PATHS = (Path(_) for _ in ("src", "tests", "duties.py", "docs"))
 PY_SRC_LIST = tuple(str(_) for _ in PY_SRC_PATHS)
 PY_SRC = " ".join(PY_SRC_LIST)
 TESTING = os.environ.get("TESTING", "0") in {"1", "true"}
@@ -40,7 +42,6 @@ def update_changelog(
     marker: str,
     version_regex: str,
     template_url: str,
-    commit_style: str,
 ) -> None:
     """
     Update the given changelog file in place.
@@ -50,15 +51,16 @@ def update_changelog(
         marker: The line after which to insert new contents.
         version_regex: A regular expression to find currently documented versions in the file.
         template_url: The URL to the Jinja template used to render contents.
-        commit_style: The style of commit messages to parse.
     """
     from git_changelog.build import Changelog
+    from git_changelog.commit import AngularStyle
     from jinja2.sandbox import SandboxedEnvironment
 
+    AngularStyle.DEFAULT_RENDER.insert(0, AngularStyle.TYPES["build"])
     env = SandboxedEnvironment(autoescape=False)
     template_text = urlopen(template_url).read().decode("utf8")  # noqa: S310
     template = env.from_string(template_text)
-    changelog = Changelog(".", style=commit_style)
+    changelog = Changelog(".", style="angular")
 
     if len(changelog.versions_list) == 1:
         last_version = changelog.versions_list[0]
@@ -98,14 +100,13 @@ def changelog(ctx):
             "marker": "<!-- insertion marker -->",
             "version_regex": r"^## \[v?(?P<version>[^\]]+)",
             "template_url": template_url,
-            "commit_style": "angular",
         },
         title="Updating changelog",
         pty=PTY,
     )
 
 
-@duty(pre=["check_code_quality", "check_types", "check_docs", "check_dependencies"])
+@duty(pre=["check_quality", "check_types", "check_docs", "check_dependencies"])
 def check(ctx):
     """
     Check it all!
@@ -116,7 +117,7 @@ def check(ctx):
 
 
 @duty
-def check_code_quality(ctx, files=PY_SRC):
+def check_quality(ctx, files=PY_SRC):
     """
     Check the code quality.
 
@@ -135,49 +136,38 @@ def check_dependencies(ctx):
     Arguments:
         ctx: The context instance (passed automatically).
     """
-    nofail = False
-    safety = which("safety")
-    if not safety:
-        pipx = which("pipx")
-        if pipx:
-            safety = f"{pipx} run safety"
-        else:
-            safety = "safety"
-            nofail = True
-    ctx.run(
-        f"pdm export -f requirements --without-hashes | {safety} check --stdin --full-report",
-        title="Checking dependencies",
-        pty=PTY,
-        nofail=nofail,
+    # undo possible patching
+    # see https://github.com/pyupio/safety/issues/348
+    for module in sys.modules:  # noqa: WPS528
+        if module.startswith("safety.") or module == "safety":
+            del sys.modules[module]  # noqa: WPS420
+
+    importlib.invalidate_caches()
+
+    # reload original, unpatched safety
+    from safety.formatter import report
+    from safety.safety import check as safety_check
+    from safety.util import read_requirements
+
+    # retrieve the list of dependencies
+    requirements = ctx.run(
+        ["pdm", "export", "-f", "requirements", "--without-hashes"],
+        title="Exporting dependencies as requirements",
+        allow_overrides=False,
     )
 
+    # check using safety as a library
+    def safety():  # noqa: WPS430
+        packages = list(read_requirements(StringIO(requirements)))
+        vulns = safety_check(packages=packages, ignore_ids="", key="", db_mirror="", cached=False, proxy={})
+        output_report = report(vulns=vulns, full=True, checked_packages=len(packages))
+        if vulns:
+            print(output_report)
 
-def no_docs_py36(nofail=True):
-    """
-    Decorate a duty that builds docs to warn that it's not possible on Python 3.6.
-
-    Arguments:
-        nofail: Whether to fail or not.
-
-    Returns:
-        The decorated function.
-    """
-
-    def decorator(func):
-        @wraps(func)
-        def wrapper(ctx):
-            if sys.version_info <= (3, 7, 0):
-                ctx.run(["false"], title="Docs can't be built on Python 3.6", nofail=nofail, quiet=True)
-            else:
-                func(ctx)
-
-        return wrapper
-
-    return decorator
+    ctx.run(safety, title="Checking dependencies")
 
 
 @duty
-@no_docs_py36()
 def check_docs(ctx):
     """
     Check if the documentation builds correctly.
@@ -190,15 +180,57 @@ def check_docs(ctx):
     ctx.run("mkdocs build -s", title="Building documentation", pty=PTY)
 
 
-@duty
-def check_types(ctx):
+@duty  # noqa: WPS231
+def check_types(ctx):  # noqa: WPS231
     """
     Check that the code is correctly typed.
 
     Arguments:
         ctx: The context instance (passed automatically).
     """
-    ctx.run(f"mypy --config-file config/mypy.ini {PY_SRC}", title="Type-checking", pty=PTY)
+    # NOTE: the following code works around this issue:
+    # https://github.com/python/mypy/issues/10633
+
+    # compute packages directory path
+    py = f"{sys.version_info.major}.{sys.version_info.minor}"
+    pkgs_dir = Path("__pypackages__", py, "lib").resolve()
+
+    # build the list of available packages
+    packages = {}
+    for package in pkgs_dir.glob("*"):
+        if package.suffix not in {".dist-info", ".pth"} and package.name != "__pycache__":
+            packages[package.name] = package
+
+    # handle .pth files
+    for pth in pkgs_dir.glob("*.pth"):
+        with suppress(OSError):
+            for package in Path(pth.read_text().splitlines()[0]).glob("*"):  # noqa: WPS440
+                if package.suffix != ".dist-info":
+                    packages[package.name] = package
+
+    # create a temporary directory to assign to MYPYPATH
+    with tempfile.TemporaryDirectory() as tmpdir:
+
+        # symlink the stubs
+        ignore = set()
+        for stubs in (path for name, path in packages.items() if name.endswith("-stubs")):  # noqa: WPS335
+            Path(tmpdir, stubs.name).symlink_to(stubs, target_is_directory=True)
+            # try to symlink the corresponding package
+            # see https://www.python.org/dev/peps/pep-0561/#stub-only-packages
+            pkg_name = stubs.name.replace("-stubs", "")
+            if pkg_name in packages:
+                ignore.add(pkg_name)
+                Path(tmpdir, pkg_name).symlink_to(packages[pkg_name], target_is_directory=True)
+
+        # create temporary mypy config to ignore stubbed packages
+        newconfig = Path("config", "mypy.ini").read_text()
+        newconfig += "\n" + "\n\n".join(f"[mypy-{pkg}.*]\nignore_errors=true" for pkg in ignore)
+        tmpconfig = Path(tmpdir, "mypy.ini")
+        tmpconfig.write_text(newconfig)
+
+        # set MYPYPATH and run mypy
+        os.environ["MYPYPATH"] = tmpdir
+        ctx.run(f"mypy --config-file {tmpconfig} {PY_SRC}", title="Type-checking", pty=PTY)
 
 
 @duty(silent=True)
@@ -223,7 +255,6 @@ def clean(ctx):
 
 
 @duty
-@no_docs_py36(nofail=False)
 def docs(ctx):
     """
     Build the documentation locally.
@@ -235,7 +266,6 @@ def docs(ctx):
 
 
 @duty
-@no_docs_py36(nofail=False)
 def docs_serve(ctx, host="127.0.0.1", port=8000):
     """
     Serve the documentation (localhost:8000).
@@ -249,7 +279,6 @@ def docs_serve(ctx, host="127.0.0.1", port=8000):
 
 
 @duty
-@no_docs_py36(nofail=False)
 def docs_deploy(ctx):
     """
     Deploy the documentation on GitHub pages.
@@ -294,7 +323,7 @@ def release(ctx, version):
         ctx.run("git push --tags", title="Pushing tags", pty=False)
         ctx.run("pdm build", title="Building dist/wheel", pty=PTY)
         ctx.run("twine upload --skip-existing dist/*", title="Publishing version", pty=PTY)
-        docs_deploy.run()  # type: ignore
+        docs_deploy.run()
 
 
 @duty(silent=True)
